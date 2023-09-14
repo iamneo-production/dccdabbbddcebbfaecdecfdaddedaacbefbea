8e5f5d4f2b948a8fe94254405f88b8e18e313085
Iamneo <techadmin@iamneo.ai>
2023-09-14 09:10:16 +0000

    Backup Commit

--- s3.sh
+++
@@ -1,4 +0,0 @@
-#mkdir new_folder1
-#touch new_file2.txt
-chmod 444 new_folder1
-chmod 444 new_file2.txt
---
+++ s4.sh
@@ -0,0 +1,5 @@
+mkdir new_folder1
+touch new_file2.txt
+chmod 444 new_folder1
+chmod 444 new_file2.txt\
+mk
