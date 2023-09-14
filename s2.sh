b17413000788f405ce702e92472eb377f0fec214
Iamneo <techadmin@iamneo.ai>
2023-09-14 08:45:16 +0000

    Backup Commit

--- s2.sh
+++ s2.sh
@@ -8,3 +8,15 @@ echo "Created folder: $folder_name"
 touch "$home/$folder_name/$file_name"
 chmod 444 "$home/$folder_name/$file_name"
 echo "Created file: $file_name"
+
+folder_name1="folder2.txt"
+file_name2="file3.txt"
+
+mkdir "$home/$folder_name1"
+chmod 644 "$home/$folder_name1"
+echo "Created folder: $folder_name1"
+
+touch "$home/$folder_name/$file_name"
+chmod 444 "$home/$folder_name/$file_name"
+echo "Created file: $file_name"
+
